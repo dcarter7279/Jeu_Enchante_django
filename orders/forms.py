cace50fb213bb36fdda0f025dff75366989a458a
dcarter <dcarter7279@gmail.com>
2022-02-20 21:06:06 -0500

    product order and payments

--- orders/forms.py
+++ orders/forms.py
@@ -3,6 +3,6 @@ from .models import Order
 
 
 class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'city', 'state', 'zip_code', 'order_note']
+	class Meta:
+		model = Order
+		fields = ['first_name', 'last_name', 'phone', 'email', 'address_line_1', 'address_line_2', 'city', 'state', 'zip_code', 'order_note']

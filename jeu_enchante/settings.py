41977087909f9103ebc340c2b0f054e18c112f24
dcarter <dcarter7279@gmail.com>
2022-03-04 08:10:44 -0500

    product gallery

--- jeu_enchante/settings.py
+++ jeu_enchante/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+from email.policy import default
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0=ol9e_=48t9#zd$f%lhcultb3nn*w72$_l#0%iyht*_k_qq(r'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +44,7 @@ INSTALLED_APPS = [
     'store',
     'carts',
     'orders',
+    'admin_honeypot',
     
     
     
@@ -56,7 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
 ]
+
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+SESSION_TIMEOUT_REDIRECT = 'accounts/login'
 
 ROOT_URLCONF = 'jeu_enchante.urls'
 
@@ -148,8 +156,8 @@ MESSAGE_TAGS = {
 
 
 # SMTP configuration
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587 
-EMAIL_HOST_USER = 'dcarter7279@gmail.com'
-EMAIL_HOST_PASSWORD = 'nwwughmvphbbnkll'
-EMAIL_USE_TLS = True
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = ('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = ('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
--- store/views.py
+++ store/views.py
@@ -1,11 +1,10 @@
 from store.forms import ReviewForm
 from django.http import HttpResponse
-from sympy import product
 from category.models import Category
 from carts.models import CartItem
 from django.db.models import Q 
 from django.shortcuts import get_object_or_404, render, redirect
-from .models import Product, ReviewRating
+from .models import Product, ProductGallery, ReviewRating
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib import messages
@@ -56,11 +55,15 @@ def product_detail(request, category_slug, product_slug):
 	# Get the reviews
 	reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+	# Get the product gallery
+	product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
 	context = {
 		'single_product': single_product,
 		'in_cart'		: in_cart,
 		'orderproduct': orderproduct,
 		'reviews': reviews,
+		'product_gallery': product_gallery,
 	}
 	return render(request, 'store/product_detail.html', context)
 
